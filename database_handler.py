94147e724bfc8a204f6c05046286639fa2dc331e
srjsnehanshu <57353992+srjsnehanshu@users.noreply.github.com>
2020-10-23 23:37:42 +0530

    follower following completed

--- database_handler.py
+++ database_handler.py
@@ -36,41 +36,115 @@ def updating_user_info(user_id, followers, followings):
         new_followings = (set(followings) - set(old_followings))
         new_followers = (set(followers) - set(old_followers))
 
-        print(new_followers)
-        print(new_followings)
-
         lost_followings = (set(old_followings) - set(followings))
         lost_followers = (set(old_followers) - set(followers))
 
-        print(lost_followers)
-        print(lost_followings)
+        fans = (set(followers) - set(followings))
+
+        print("Select options : ")
+        while(True):
+            print("1: Not Follow Back \n2: New Following \n3: New Followers \n4: Lost Followers \n5: Lost Followings \n6. Fans \n7: Exit")
+            option = int(input())
+            if(option == 1):
+                print("Not Follow back: ")
+                not_follow_back = (set(followings) - set(followers))
+                print(len(not_follow_back))
+                for users in not_follow_back:
+                    print(users)
+            elif(option == 2):
+                print("New Followings: ")
+                print(len(new_followings))
+                for users in new_followings:
+                    print(users)
+            elif(option == 3):
+                print("New Followers: ")
+                print(len(new_followers))
+                for users in new_followers:
+                    print(users)
+            elif(option == 4):
+                print("Lost Followers: ")
+                print(len(lost_followers))
+                for users in lost_followers:
+                    print(users)
+            elif(option == 5):
+                print("Lost Followings: ")
+                print(len(lost_followings))
+                for users in lost_followings:
+                    print(users)
+            elif(option == 6):
+                print("Fans:")
+                print(len(fans))
+                for users in fans:
+                    print(users)
+            elif(option == 7):
+                break
+            else:
+                print("Enter a valid option!")
 
         for names in new_followers:
-            my_cursor.execute("INSERT INTO follower_following(follower_id, following_id) VALUES(%s, %s)", (names, user_id,))
+            try:
+                my_cursor.execute("INSERT INTO follower_following(follower_id, following_id) VALUES(%s, %s)", (names, user_id,))
+            except:
+                continue
 
         for names in new_followings:
-            my_cursor.execute("INSERT INTO follower_following(follower_id, following_id) VALUES(%s, %s)", (user_id, names,))
+            try:
+                my_cursor.execute("INSERT INTO follower_following(follower_id, following_id) VALUES(%s, %s)", (user_id, names,))
+            except:
+                continue
 
         for names in lost_followers:
-            my_cursor.execute("delete from follower_following where following_id = %s and follower_id = %s", (user_id, names,))
-        
+            try:
+                my_cursor.execute("delete from follower_following where following_id = %s and follower_id = %s", (user_id, names,))
+            except:
+                continue
+
         for names in lost_followings:
-            my_cursor.execute("delete from follower_following where following_id = %s and follower_id = %s", (names, user_id,))
+            try:
+                my_cursor.execute("delete from follower_following where following_id = %s and follower_id = %s", (names, user_id,))
+            except:
+                continue
 
         mydb.commit()
+
     else:
         print("You are a new user")
 
         my_cursor.execute("insert into users(user_id) value(%s)", (user_id,))
 
-        for names in followers:
-            my_cursor.execute("INSERT INTO follower_following(follower_id, following_id) VALUES(%s, %s)", (names, user_id,))  
+        print("Select Option: \n1.Not Follow Back \n2.Fans \n3.Exit")
+        while(True):
+            option = int(input())
+            if(option == 1):
+                print("Not Follow back: ")
+                not_follow_back = (set(followings) - set(followers))
+                print(len(not_follow_back))
+                for users in not_follow_back:
+                    print(users)
+            elif(option == 2):
+                print("Fans: ")
+                fans = (set(followers) - set(followings))
+                print(len(fans))
+                for users in fans:
+                    print(users)
+            elif(option == 3):
+                break
+            else:
+                print("Enter valid option!")
 
-        for names in followings:
-            my_cursor.execute("INSERT INTO follower_following(follower_id, following_id) VALUES(%s, %s)", (user_id, names,))
+            for names in followers:
+                try:
+                    my_cursor.execute("INSERT INTO follower_following(follower_id, following_id) VALUES(%s, %s)", (names, user_id,))  
+                except:
+                    continue
 
-        print("Not Follow back: ")
-        nfb = (set(followings) - set(followers))
-        print(nfb)
-        
-        mydb.commit()
+            for names in followings:
+                try:
+                    my_cursor.execute("INSERT INTO follower_following(follower_id, following_id) VALUES(%s, %s)", (user_id, names,))
+                except:
+                    continue
+            
+            mydb.commit()
+
+    
+
--- instalogin.py
+++ instalogin.py
@@ -1,9 +1,12 @@
 #C:\Users\Snehanshu Raj\Downloads\chromedriver_win32\chromedriver.exe
 
+from tqdm import tqdm
 import database_handler
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import requests
 import time
+import getpass
 
 chromePath = r"C:\Users\Snehanshu Raj\Downloads\chromedriver_win32\chromedriver.exe"
 
@@ -19,7 +22,7 @@ user_id = input("Enter username or phone number: ")
 driver.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div/div[1]/div/label/input').send_keys(user_id)
 
 time.sleep(2)
-password = input("Enter Password: ")
+password = getpass.getpass()
 driver.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div/div[2]/div/label/input').send_keys(password)
 
 credentials = False
@@ -75,6 +78,8 @@ if(logged_in == True):
     driver.get(main_page_url)
     print("You are now on your profile page...")
 
+    time.sleep(2)
+
     #accessing followers and following
     number_of_followers = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/span').text
     number_of_followings = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[3]/a/span').text
@@ -86,25 +91,24 @@ if(logged_in == True):
     followers = int(input("Enter number of followers: "))
     followings = int(input("Enter number of followings: "))
 
+    print("Loading information...Please be patient!")
     #accessing followers
     driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[2]/a').click()
     time.sleep(2)
     fBody = driver.find_element_by_css_selector("div[class='isgrP']")
-    scrolling_times = ((followers) / 4)
+    scrolling_times = ((followers) // 5)
     scroll = 0
-    scroll_count = scrolling_times + 5  
-    while scroll < scroll_count:
+    scroll_count = scrolling_times 
+    for scroll in tqdm(range(scroll_count), desc="Fetching Followers"):
         driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', fBody)
         time.sleep(2)
-        scroll += 1
-    
+
     a_list = driver.find_elements_by_tag_name('a')
     followers_list = []
 
-    for f_name in a_list:
-        if(len(f_name.get_attribute('title')) > 0):
-            followers_list.append(f_name.get_attribute('title'))
-    print(followers_list)
+    for i in tqdm(range(len(a_list)), desc="Updating your followers data"):
+        if(len(a_list[i].get_attribute('title')) > 0):
+            followers_list.append(a_list[i].get_attribute('title'))
 
     #accessing followings
     driver.get(main_page_url)
@@ -113,24 +117,83 @@ if(logged_in == True):
     driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[3]/a').click()
     time.sleep(2)
     fBody = driver.find_element_by_css_selector("div[class='isgrP']")
-    scrolling_times = ((followings) / 4)
+    scrolling_times = ((followings) // 5)
     scroll = 0
-    scroll_count = scrolling_times + 5  
-    while scroll < scroll_count:
+    scroll_count = scrolling_times
+    for scroll in tqdm(range(scroll_count), desc="Fetching Followings"):
         driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', fBody)
         time.sleep(2)
-        scroll += 1
     
     b_list = driver.find_elements_by_tag_name('a')
     followings_list = []
 
-    for f_name in b_list:
-        if(len(f_name.get_attribute('title')) > 0):
-            followings_list.append(f_name.get_attribute('title'))
-    print(followings_list)
+    for i in tqdm(range(len(b_list)), desc="Updating your followings data"):
+        if(len(b_list[i].get_attribute('title')) > 0):
+            followings_list.append(b_list[i].get_attribute('title'))
 
+    print("Loaded Information Successfully!")
     #updating user info
     database_handler.updating_user_info(user_id, followers_list, followings_list)
 
+    print("Analayze posts?")
+    option = input()
+
+    if(option == 'y' or option == 'Y'):
+        no_of_posts = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[1]/span/span').text
+        print("Total Posts: ")
+        print(no_of_posts)
+        print("Enter Number of Posts: ")
+        no_posts = int(input())
+
+        scrolling_times = ((no_posts) // 5)
+        scroll = 0
+        scroll_count = scrolling_times 
+
+        links = set()
+        for scroll in tqdm(range(scroll_count), desc="Analyzing Page"):
+            box = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div[3]/article')
+            posts = box.find_elements_by_tag_name('a')
+            for i in range(len(posts)):
+                links.add(posts[i].get_attribute('href'))
+            if(len(links) == no_posts):
+                break
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)
+
+        video_counts = 1
+        display_likes = 0
+        actual_likes = 0
+
+        for link in links:
+            driver.get(link)
+            likers = set()
+
+            time.sleep(2)
+            try:
+                likes = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[2]/div/div[2]/button/span').text
+                display_likes += (int(likes.rstrip("%")))
+                driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[2]/div/div[2]/button').click()
+                #driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[2]/div/div/button').click()
+                time.sleep(2)
+                fBody = driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/div')
+                tbody = driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/div/div')
+                scrolling_times = (int(likes) // 5)
+                scroll = 0
+                scroll_count = scrolling_times 
+                for scroll in tqdm(range(scroll_count), desc="Fetching Likers"):
+                    driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', fBody)
+                    time.sleep(2)
+                    all_likers = tbody.find_elements_by_tag_name('a')
+                    for i in range(len(all_likers)):
+                        if(len(all_likers[i].get_attribute('title')) > 0):
+                            likers.add(all_likers[i].get_attribute('title'))
+        
+                actual_likes += len(likers)
+            except:
+                print(f"Found {video_counts} video which is not being analyzed...")
+                video_counts += 1
+    else:
+        print("Thanks for using this application")
+        
 else:
     print("Logged in fail!! Please try again with valid credentials")
